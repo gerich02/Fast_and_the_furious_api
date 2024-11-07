9b27e07d70e007a71c09acbf457acd1701be5ac9
unknown <ger-adv@yandex.ru>
2024-11-07 09:45:11 +0300

    Добавил поддержку миграций alembic

--- app/database.py
+++ app/database.py
@@ -1,8 +1,10 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_URL = f"sqlite:///{os.path.abspath('sql_app.db')}"
 
 engine = create_engine(
     SQLALCHEMY_URL,
